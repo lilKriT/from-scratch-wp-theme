7eadab675be096624d22ec803fbca200f3ddf188
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-24 19:13:23 +0200

    Created the loop.

--- front-page.php
+++ front-page.php
@@ -2,7 +2,16 @@
 get_header();
 ?>
 <article class="content px-3 py-5 p-md-5">
-    asdf
+
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+            the_content();
+        }
+    }
+    ?>
+
 </article>
 
 <?php
