66acccd541875baaf47e9349bd96c471d2b67bbf
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-24 21:34:21 +0200

    Added dynamic logo and title.

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function lk_theme_support()
 {
     // Add dynamic title tag support
     add_theme_support('title-tag');
+    add_theme_support('custom-logo');
 }
 add_action('after_setup_theme', 'lk_theme_support');
 
