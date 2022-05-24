0b21df9d669535084dee33ebd53b05154c96d7ae
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-24 18:48:48 +0200

    Worked on footer and header.

--- functions.php
+++ functions.php
@@ -16,6 +16,6 @@ function lk_register_scripts()
     wp_enqueue_script('followandrew-jquery', "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), '3.4.1', true);
     wp_enqueue_script('followandrew-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.0', true);
     wp_enqueue_script('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '4.4.1', true);
-    wp_enqueue_script('followandrew-bootstrap', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
+    wp_enqueue_script('followandrew-main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'lk_register_scripts');
