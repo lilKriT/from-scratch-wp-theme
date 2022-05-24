87871c50c632512a6d19e4f165e592acd227c505
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-25 00:01:13 +0200

    Added post thumbnails.

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function lk_theme_support()
     // Add dynamic title tag support
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'lk_theme_support');
 
